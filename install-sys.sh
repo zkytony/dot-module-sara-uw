46c005de74aaea83130b599996f22e6f530e67cb
Andrzej Pronobis <a.pronobis@gmail.com>
2015-02-06 22:45:16 -0800

    .

--- install-sys.sh
+++ install-sys.sh
@@ -12,6 +12,9 @@ set -e
 # Import tools
 . $DOT_DIR/shell/tools.bash
 
+# Check if run as root
+check_root
+
 # Header
 print_main_module_header
 
@@ -21,22 +24,8 @@ print_main_module_header
 ## -------------------------------------------------------------
 
 ## -------------------------------------------------------------
-print_header "Creating links to binaries"
-# dot_link_bin $DOT_MODULE_DIR """
-print_status "Done!"
-
-
-## -------------------------------------------------------------
-print_header "Installing user-local config files"
-# VNC client
-dot_link_config $DOT_MODULE_DIR ".vnc/profiles/sara_uw_dube.vnc"
-dot_link_config $DOT_MODULE_DIR ".vnc/profiles/sara_uw_ec2_sim.vnc"
-# Shortcuts
-dot_link_config $DOT_MODULE_DIR ".local/share/applications/*.desktop"
-# Art
-dot_link_config $DOT_MODULE_DIR ".local/share/icons/*.png"
-dot_link_config $DOT_MODULE_DIR ".local/share/wallpapers/*.png"
-# Done
+print_header "Installing Ubuntu packages"
+sudo apt-get install ccache
 print_status "Done!"
 
 
