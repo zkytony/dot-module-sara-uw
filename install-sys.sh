32dc41da32ab6d75c4880f860c8420de45a23245
Andrzej Pronobis <a.pronobis@gmail.com>
2015-02-10 04:33:28 +0000

    Installing ROS

--- install-sys.sh
+++ install-sys.sh
@@ -40,6 +40,33 @@ print_status "Done!"
 
 
 ## -------------------------------------------------------------
+print_header "Installing ROS"
+if [ "$(lsb_release -cs)" == "trusty" ]
+then
+    print_status "Installing ROS using Ubuntu Trusty packages"
+    #
+    print_status "\nAdding ROS repositories..."
+    sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
+    wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
+    apt-get update
+    #
+    print_status "\nInstalling ROS packages..."
+    sudo apt-get install ros-indigo-catkin ros-indigo-ros python-wstool
+    #
+    print_status "\nInstalling rosdep..."
+    if [ -f /etc/ros/rosdep/sources.list.d/20-default.list ]
+    then
+        rm /etc/ros/rosdep/sources.list.d/20-default.list
+    fi
+    rosdep init
+    # Done!
+    print_status "Done!"
+else
+    print_warning "You are not running Ubuntu 14.04 Trusty. Your ROS will be installed from source."
+fi
+
+
+## -------------------------------------------------------------
 ## Finishing
 ## -------------------------------------------------------------
 print_main_module_footer
