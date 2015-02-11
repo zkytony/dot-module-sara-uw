86c19f7e22482d30b9145df25c5cace78391dcf9
Andrzej Pronobis <a.pronobis@gmail.com>
2015-02-10 21:00:03 -0800

    Robot system

---
+++ systems/10_sara_uw_dube/setup.bash
@@ -0,0 +1,20 @@
+# SARA
+if [ -d $SARA_ROOT ]
+then
+    source $SARA_ROOT/sara_ws/devel/setup.bash
+fi
+
+# ROS configuration
+export ROSCONSOLE_FORMAT='[${severity}] [${node}@${function}:${line}]: ${message}'
+
+# MIRA configuration
+export MIRA_PATH=/opt/MIRA:/opt/MIRA-commercial
+export LD_LIBRARY_PATH=/opt/MIRA/lib:${LD_LIBRARY_PATH}
+export PATH=/opt/MIRA/bin:${PATH}
+export MIRA_PATH=/opt/MIRA-commercial:${MIRA_PATH}
+export LD_LIBRARY_PATH=/opt/MIRA-commercial/lib:${LD_LIBRARY_PATH}
+source /opt/MIRA/scripts/mirabash
+
+# ROS networking
+export ROS_MASTER_URI=http://128.208.7.254:11311
+export ROS_HOSTNAME=128.208.7.254
