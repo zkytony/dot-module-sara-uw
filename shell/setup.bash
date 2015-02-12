934d4a6f522aa4700abd5bdadd57fbc9969e7f3b
Andrzej Pronobis <a.pronobis@gmail.com>
2015-02-12 12:31:39 -0800

    Added java detection

--- shell/setup.bash
+++ shell/setup.bash
@@ -22,3 +22,21 @@ export PATH=$PATH:${DOT_MODULE_DIR}/opt/ec2-api-tools/bin
 export EC2_URL=https://ec2.us-west-2.amazonaws.com
 # Aliases
 alias ec2-status="ec2-describe-instance-status -AH | awk '{print \$2 \"\\t\" \$4}'"
+
+## ---------------------------
+## Java settings
+## ---------------------------
+if [ -z "$JAVA_HOME" ]
+then
+    # Detect java, use oracle 7 first, then whatever the default is, and then try openjdk 1.7
+    if [ -d /usr/lib/jvm/java-7-oracle ]
+    then
+        export JAVA_HOME="/usr/lib/jvm/java-7-oracle"
+    elif [ -d /usr/lib/jvm/default-java ]
+    then
+        export JAVA_HOME="/usr/lib/jvm/default-java"
+    elif [ -d /usr/lib/jvm/java-1.7.0-openjdk-amd64 ]
+    then
+        export JAVA_HOME="/usr/lib/jvm/java-1.7.0-openjdk-amd64"
+    fi
+fi
