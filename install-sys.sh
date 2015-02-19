c0c912848262e817fb31ff220666a1461899483b
Andrzej Pronobis <a.pronobis@gmail.com>
2015-02-18 21:49:53 -0800

    Fixed cuda init

--- install-sys.sh
+++ install-sys.sh
@@ -42,8 +42,8 @@ print_status "Done!"
 ## -------------------------------------------------------------
 print_header "Installing system-wide config files"
 # CUDA Initialization (harmless on systems without NVidia)
-dot_copy_config_sys $DOT_MODULE_DIR "etc/init.d/cuda_init"
-dot_copy_config_sys $DOT_MODULE_DIR "etc/rc2.d/S99cuda_init"
+dot_copy_config_sys $DOT_MODULE_DIR "etc/init.d/cuda-init"
+dot_copy_config_sys $DOT_MODULE_DIR "etc/rc2.d/S99cuda-init"
 chmod a+x /etc/init.d/cuda_init
 # Done
 print_status "Done!"
