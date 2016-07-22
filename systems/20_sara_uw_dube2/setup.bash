9b37be8a05e9becb4d5745638942bb0ca06b71e1
Andrzej Pronobis <a.pronobis@gmail.com>
2016-07-21 21:02:44 -0700

    Updated systems

---
+++ systems/20_sara_uw_dube2/setup.bash
@@ -0,0 +1,26 @@
+# -*- mode: sh -*-
+## ----------------------------------------------------------
+## Executed for interactive, login and non-login
+## Bash sessions.
+## ----------------------------------------------------------
+
+# SARA
+if [ -d $SARA_ROOT ]
+then
+    source $SARA_ROOT/sara_ws/devel/setup.bash
+fi
+
+# ROS configuration
+export ROSCONSOLE_FORMAT='[${severity}] [${node}@${function}:${line}]: ${message}'
+
+# MIRA configuration
+export MIRA_PATH=/opt/MIRA
+export LD_LIBRARY_PATH=/opt/MIRA/lib:${LD_LIBRARY_PATH}
+export PATH=/opt/MIRA/bin:${PATH}
+export MIRA_PATH=/opt/MIRA-commercial:${MIRA_PATH}
+export LD_LIBRARY_PATH=/opt/MIRA-commercial/lib:${LD_LIBRARY_PATH}
+export MIRA_PATH=${SARA_ROOT}/sara_ws/src:${MIRA_PATH}
+
+# ROS networking
+export ROS_MASTER_URI=http://128.208.7.254:11311
+export ROS_HOSTNAME=172.28.7.105
